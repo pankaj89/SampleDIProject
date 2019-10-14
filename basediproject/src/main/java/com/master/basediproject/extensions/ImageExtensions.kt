7121ce32dd18a5f90b4f545fa84d27e9cdc1fd7d
Pankaj <pankaj.sharma@hiddenbrains.in>
2019-10-14 19:05:56 +0530

    Added multitransformation in glide while loading image

--- basediproject/src/main/java/com/master/basediproject/extensions/ImageExtensions.kt
+++ basediproject/src/main/java/com/master/basediproject/extensions/ImageExtensions.kt
@@ -4,6 +4,8 @@ import android.widget.ImageView
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.MultiTransformation
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
@@ -33,7 +35,7 @@ fun ImageView.loadImageWithCircle(url: String?, placeHolder: Int = -1) {
 fun ImageView.loadImageWithRoundedCorner(url: String?, placeHolder: Int = -1, radius: Int = 0) {
 
 //    url.ifNotBlank {
-        val requestManager = Glide.with(this).load(url).apply(RequestOptions.bitmapTransform(RoundedCorners(radius)))
+        val requestManager = Glide.with(this).load(url).transform(MultiTransformation(CenterCrop(), RoundedCorners(radius)))
         if (placeHolder != -1) {
             requestManager.apply(RequestOptions().placeholder(placeHolder).error(placeHolder)).into(this)
         } else {
